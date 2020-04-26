b8cfd0cd3448958a0378c8521870a8746dfbd988
赵栋亮 <zhaodongliang@itimebox.com>
2020-04-26 16:20:30 +0800

    扫一扫完成

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '二维码扫描',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 ///主页面
-class MyHomePage extends StatelessWidget{
+///
+///
+/////
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String barcode;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("分支页面"),),
-      body: Center(child: Text("分支呀"),),
+      appBar: AppBar(
+        title: Text("扫一扫"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("$barcode"),
+          MaterialButton(
+            onPressed: scan,
+            child: Text("Scan"),
+            color: Colors.blue,
+            textColor: Colors.white,
+          ),
+        ],
+      ),
     );
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await scanner.scan();
+      setState(() => this.barcode = barcode);
+    } on Exception catch (e) {
+      if (e == scanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
   }
 }
